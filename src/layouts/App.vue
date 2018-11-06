c85587e3b53be446caacf7ac3230ca505124e685
Guilherme Beidaki <guilherme@accellog.com>
2018-11-05 22:38:09 -0200

    footer ok

--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -9,20 +9,25 @@
     </q-layout-header>
     <q-layout-footer>
       <div id="footer" class="row full-width bg-tertiary text-white text-center">
-        <div class="col-2">
+        <div class="colFooter">
           <img :src="images.sensor" alt="Sensores" class="iconFooter">
+          <span class="block fontFooterSelector">Sensores</span>
         </div>
-        <div class="col-2">
+        <div class="colFooter">
           <img :src="images.permission" alt="Sensores" class="iconFooter">
+          <span class="block fontFooterSelector">Permissões</span>
         </div>
-        <div class="col-4">
+        <div class="colFooter">
           <img :src="images.home" alt="Sensores" class="iconFooter">
+          <span class="block fontFooterSelector">Início</span>
         </div>
-        <div class="col-2">
+        <div class="colFooter">
           <img :src="images.ocorrencia" alt="Sensores" class="iconFooter">
+          <span class="block fontFooterSelector">Ocorrências</span>
         </div>
-        <div class="col-2">
+        <div class="colFooter">
           <img :src="images.config" alt="Sensores" class="iconFooter">
+          <span class="block fontFooterSelector">Configurações</span>
         </div>
       </div>
     </q-layout-footer>
@@ -51,10 +56,22 @@ export default {
 
 <style scoped>
 #footer {
-  padding: 10px;
+  padding: 10px 0;
+}
+.colFooter {
+  width: calc(100vw/5);
+  padding: 5px;
+  border-radius: 5px;
+}
+.colFooter:hover {
+  background-color: rgba(255, 255, 255, 0.2);
 }
 .iconFooter {
   width: 25px;
   height: 25px;
+}
+.fontFooterSelector {
+  font-size: calc(8px + 0.5vw);
+  font-weight: 300;
 }
 </style>
